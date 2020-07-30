76a0b2358e1235e725295f63a74048b5c69f51b9
Fadi Hijazi <fadi@yscape.com>
2020-07-30 12:09:14 +0300

    add all news files

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    dd("hello world");
     return view('welcome');
 });
-
-
-Route::get('/about', function () {
-    dd("just about . . . ");
+Route::get('/logout-manually', function () {
+    \Illuminate\Support\Facades\Auth::logout();
+    return redirect('/');
 });
+Auth::routes();
 
-Route::get('/team', function () {
-   return "team work";
-});
-
-Route::get('/toGitHup', function () {
-    return "to githup ...";
-});
-Route::resource('/test', 'TestController');
+Route::get('/home', 'HomeController@index')->name('home');
