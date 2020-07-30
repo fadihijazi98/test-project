4b70900f8f4abe9002d08a1c701ce11a6e667dec
Fadi Hijazi <fadi@yscape.com>
2020-07-30 15:02:23 +0300

    seconde modified files jul 30 15:02

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,16 @@
+import Vue from "vue";
+import AuthorizedClients from "./components/passport/AuthorizedClients";
+import PersonalAccessTokens from "./components/passport/PersonalAccessTokens";
+import TestComponent from "./components/TestComponent";
+import Clients from "./components/passport/Clients";
 require('./bootstrap');
 
+const app = new Vue({
+    el: "#app",
+    components: {
+      AuthorizedClients,
+      PersonalAccessTokens,
+      Clients,
+      TestComponent,
+    },
+});
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Test Component .1 .2 .3
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TestComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
